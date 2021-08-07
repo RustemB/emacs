f38779df7e9c21229d91cafe8750a6bad56085f3
RustemB <rustemb@systemli.org>
2021-08-07 22:03:05 +0500

    added lsp support for prolog-mode

--- init.el
+++ init.el
@@ -51,7 +51,19 @@
 	 (sh-mode . lsp))
   :config
   (setq lsp-rust-library-directories '("~/.config/cargo/registry/src"
-				       "~/.config/rustup/toolchains")))
+				       "~/.config/rustup/toolchains"))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection
+    (lsp-stdio-connection (list "swipl"
+				"-g" "use_module(library(lsp_server))."
+				"-g" "lsp_server:main"
+				"-t" "halt"
+				"--" "stdio"))
+    :major-modes '(prolog-mode)
+    :priority 1
+    :multi-root t
+    :server-id 'prolog-ls)))
 
 (use-package magit
   :ensure t)
